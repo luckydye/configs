0eb48b8118fff42fcdbb44e0412070f6c5ec5603
luckydye <t.havlicek@s-v.de>
2023-04-02 23:01:27 +0200

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,16 @@
+# install ansible and setup things
 
+pkgmngr=$(package_manager)
+
+case $pkgmngr in
+
+  apt)
+    sudo apt -y update
+    sudo apt -y install ansible
+    play setup_ubuntu.yml
+    ;;
+
+  *)
+    echo -n "unknown package manager"
+    ;;
+esac
