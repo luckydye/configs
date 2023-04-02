c5317d55f3c6048f61fbbd1adac490181cf6d976
luckydye <t.havlicek@s-v.de>
2023-04-02 21:52:27 +0200

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,6 +2,6 @@
 
 git clone https://github.com/luckydye/configs.git ~/configs
 
-echo "source ~/configs/.bashrc" > ~/.bashrc
+echo "source ~/configs/.bashrc" >> ~/.bashrc
 
 echo "Successfully installed."
