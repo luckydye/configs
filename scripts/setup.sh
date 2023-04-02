a20d06afb172d64e863d20e084920f24570f34de
luckydye <t.havlicek@s-v.de>
2023-04-02 23:03:37 +0200

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,11 @@
 # install ansible and setup things
 
+function package_manager {
+  which yum > /dev/null && { echo "yum"; return; }
+  which apk > /dev/null && { echo "apk"; return; }
+  which apt > /dev/null && { echo "apt"; return; }
+}
+
 pkgmngr=$(package_manager)
 
 case $pkgmngr in
