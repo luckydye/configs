52e70ee0089fb1a3ab31d24ee59e7e0be8342bae
luckydye <t.havlicek@s-v.de>
2023-04-03 15:43:08 +0200

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,11 +1,33 @@
 # linux machine first setup
 
+function package_manager {
+  which yum > /dev/null && { echo "yum"; return; }
+  which apk > /dev/null && { echo "apk"; return; }
+  which apt > /dev/null && { echo "apt"; return; }
+  which brew > /dev/null && { echo "brew"; return; }
+}
+
 if ! command -v git &> /dev/null
+    pkgmngr=$(package_manager)
+
+    case $pkgmngr in
+
+      apt)
+        sudo apt -y update
+        sudo apt -y install git
+        ;;
+
+      brew)
+        brwe install git
+        ;;
+
+      *)
+        echo -n "unknown package manager"
+        ;;
+    esac
+        exit
+    fi
 then
-    sudo apt update
-    sudo apt install git
-    exit
-fi
 
 git clone https://github.com/luckydye/configs.git ~/configs
 
