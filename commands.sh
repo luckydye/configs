3ab2e564654a077f8d2fad123225f6d1dc097da5
luckydye <t.havlicek@s-v.de>
2023-04-05 21:44:40 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -11,7 +11,7 @@ alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 
-function update_configs() {
+export function update_configs() {
         echo "\tChecking for config updates.."
         prev_dir=$PWD
         cd ${CONFIGS_DIR}
@@ -24,22 +24,22 @@ export PATH="$PATH:~/bin"
 alias quit="exit"
 alias t="turbo run"
 
-function play() {
+export function play() {
         ansible-playbook ${CONFIGS_DIR}/playbooks/$1.yml
 }
 
-function run() {
+export function run() {
         SCRIPT_FILE=${CONFIGS_DIR}/scripts/$1
         find $SCRIPT_FILE 2> /dev/null && bash $SCRIPT_FILE && return
         find $SCRIPT_FILE.sh 2> /dev/null && bash $SCRIPT_FILE.sh && return
 }
 
-function find-project() {
+export function find-project() {
         echo -n "Find project: "; read project
         find ~/source -type d -name "*$project*" -maxdepth 3 -ok code {} \;
 }
 
-function package_manager {
+export function package_manager {
         which brew > /dev/null && { echo "brew"; return; }
         which yum > /dev/null && { echo "yum"; return; }
         which apk > /dev/null && { echo "apk"; return; }
