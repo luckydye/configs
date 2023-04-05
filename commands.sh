c2999a8c230c7ab597e9376f41319b8921a2a036
luckydye <t.havlicek@s-v.de>
2023-04-05 20:19:35 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -10,10 +10,10 @@ alias la='ls -A'
 alias l='ls -CF'
 
 function update_configs() {
+echo "Checking for config updates.."
         prev_dir=$PWD
         cd ~/configs
-        git pull > /dev/null
-        echo -ne "\033[2K"
+        git pull 2> /dev/null
         cd $prev_dir
 }
 
