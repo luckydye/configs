60d395dadbba490a21b3deb792f3d17d3cba0333
luckydye <t.havlicek@s-v.de>
2023-04-08 13:40:16 +0200

    Update ha.sh

--- scripts/ha.sh
+++ scripts/ha.sh
@@ -1,4 +1,1 @@
-echo $1
-echo $2
-echo curl "https://home.luckydye.de/api/$2" -X "POST" -H "Authorization: Bearer $HOME_ASSISTANT_TOKEN" -H "Content-Type: application/json" -d "{ \"entity_id\": \"$3\" }"
 curl "https://home.luckydye.de/api/$2" -X "POST" -H "Authorization: Bearer $HOME_ASSISTANT_TOKEN" -H "Content-Type: application/json" -d "{ \"entity_id\": \"$3\" }"
