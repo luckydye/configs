8adf58c90acb54f5c2905eae07db7b313d5369be
Tim <t.havlicek@s-v.de>
2023-06-25 12:49:42 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -10,6 +10,7 @@ alias adda="git add --all"
 alias pull="git pull"
 alias push="git push"
 alias commit="git commit -m"
+alias graph="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all"
 
 alias n="npm run"
 
