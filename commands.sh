0a311a06f431a8c4881ed0e49ba274d546a582dd
Tim <t.havlicek@s-v.de>
2023-06-29 14:29:16 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -9,6 +9,7 @@ alias add="git add"
 alias adda="git add --all"
 alias pull="git pull"
 alias push="git push"
+alias merge="git merge"
 alias commit="git commit -m"
 alias graph="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all"
 
