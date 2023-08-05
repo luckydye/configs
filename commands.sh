445abd2e969f0b96e2c04719555049f942f0e79e
Tim <tim.h4vlicek@gmail.com>
2023-08-05 22:23:13 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -20,7 +20,7 @@ alias graph="git log --graph --abbrev-commit --decorate --format=format:'%C(bold
 
 # docker
 alias compose="docker-compose"
-alias dd="docker run --rm -it --entrypoint "/source/devcontainer.sh" -v ~/source:/source -w /source luckydye/build-utils:latest"
+alias dd="docker run --rm -it --entrypoint "/source/devcontainer.sh" -v ~/source:/source -w /source luckydye/buildapp:latest"
 
 # node
 alias n="npm run"
