6d72ccedb89af544270e690a70eae48e7097ec2c
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-09-18 20:56:18 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -22,6 +22,7 @@ function commit() {
 }
 
 alias graph="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all"
+alias g="graph"
 
 # docker
 alias compose="docker-compose"
