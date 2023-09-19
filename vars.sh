280e7fee941e7d1e97b61d9682823e7cd792bb76
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-09-19 23:44:45 +0200

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -7,6 +7,3 @@ export CHARM_HTTP_PORT=443
 export CHARM_HOST=charm.luckydye.de
 
 export GPG_TTY=$(tty)
-
-export OPENAI_API_KEY=$(skate get openai_token)
-export HOME_ASSISTANT_TOKEN=$(skate get homeassistant_token)
