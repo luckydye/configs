31bc04d9932441d3d5dc7598426de439498f8242
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-10-02 18:10:56 +0200

    nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -47,15 +47,6 @@ return {
   -- change some telescope options and a keymap to browse plugin files
   {
     "nvim-telescope/telescope.nvim",
-    keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
-    },
     -- change some options
     opts = {
       defaults = {
