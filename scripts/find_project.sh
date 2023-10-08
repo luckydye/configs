f3eb1c7bcc8b979e6a5eb6c8841fa78e7998a0f2
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-10-08 20:34:48 +0200

    Update find_project.sh

--- scripts/find_project.sh
+++ scripts/find_project.sh
@@ -1,5 +1,5 @@
 cd $(find ~/source -maxdepth 4 -name .git -type d | sed 's/\/.git//g' | gum filter)
 
-tmux new-session -d -s htop-session 'dev-env';
+tmux new-session -d -s devenv-session 'devenv';
 tmux send 'pull' ENTER;
 tmux a;
