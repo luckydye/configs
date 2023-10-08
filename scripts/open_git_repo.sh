c1e48080e67efed522d6726060dadfa3d738caa5
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-10-08 20:51:17 +0200

    Update open_git_repo.sh

--- scripts/open_git_repo.sh
+++ scripts/open_git_repo.sh
@@ -10,7 +10,7 @@ case $provider in
     ;;
 
   "github.com")
-	repo=$(curl --request GET \
+	repo=$(curl -s --request GET \
 		--url "https://api.github.com/user/repos?per_page=100" \
 		--header "Authorization: Bearer $(skate get gh_token)" \
 		--header "X-GitHub-Api-Version: 2022-11-28" | jq -r .[].ssh_url | gum filter)
