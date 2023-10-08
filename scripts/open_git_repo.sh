b9b17161aa727e17d7064e7ebf6726cebd7ceb5d
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-10-08 20:42:31 +0200

    Update open_git_repo.sh

--- scripts/open_git_repo.sh
+++ scripts/open_git_repo.sh
@@ -2,6 +2,11 @@ repo=$(curl --request GET \
 	--url "https://api.github.com/user/repos?per_page=100" \
 	--header "Authorization: Bearer $(skate get gh_token)" \
 	--header "X-GitHub-Api-Version: 2022-11-28" | jq -r .[].ssh_url | gum filter)
+ 
+repo=$repo\n$(curl --request GET \
+	--url "https://gitlab.s-v.de/api/v4/projects?per_page=100" \
+	--header "Authorization: Bearer $(skate get gitlab_token)" \
+	--header "X-GitHub-Api-Version: 2022-11-28" | jq -r .[].ssh_url_to_rep | gum filter)
 
 cd ~/source/
 name=$(basename -s .git $repo)
