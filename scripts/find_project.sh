74efdfdf1becc0b63d72411af0cb105fcf734131
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-10-12 18:36:53 +0200

    Update find_project.sh

--- scripts/find_project.sh
+++ scripts/find_project.sh
@@ -1,5 +1,6 @@
 cd $(find ~/source -maxdepth 2 -name .git -type d | sed 's/\/.git//g' | gum filter)
 
-tmux new-session -d -s devenv-session
+id=$(uuidgen)
+tmux new-session -d -s $id
 tmux send 'pull' ENTER
-tmux a
+tmux attach -t $id
