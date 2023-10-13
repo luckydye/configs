ebed5d0faea9c0fb0a503c7907253ea29eb4a0fa
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-10-13 22:38:46 +0200

    Update config_sync.sh

--- scripts/config_sync.sh
+++ scripts/config_sync.sh
@@ -4,6 +4,13 @@ git -C $HOME/configs/ stash
 git -C $HOME/configs/ pull
 git -C $HOME/configs/ stash pop
 
+function package_manager {
+  which brew > /dev/null && { echo "brew"; return; }
+  which yum > /dev/null && { echo "yum"; return; }
+  which apk > /dev/null && { echo "apk"; return; }
+  which apt > /dev/null && { echo "apt"; return; }
+}
+
 case $(package_manager) in
   apt)
     ansible-playbook $CONFIGS_DIR/playbooks/setup_ubuntu.yml || exit 1
