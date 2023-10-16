f4aea409c21b5bc96debe7d5403e8550e654d5dc
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-10-16 22:01:18 +0200

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -11,6 +11,8 @@ export GPG_TTY=$(tty)
 
 export STARSHIP_CONFIG=$CONFIGS_DIR/starship/starship.toml
 
+export RTX_INSTALL_PATH=~/bin/rtx
+
 # COLORS
 
 # https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
