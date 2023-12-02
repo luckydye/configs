8fd10f765114d2dbaedcef893bae499d39427e02
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-12-02 14:31:32 +0100

    Update find_project.sh

--- scripts/find_project.sh
+++ scripts/find_project.sh
@@ -1,7 +1,18 @@
 project=$(find ~/source -maxdepth 2 -name .git -type d | sed 's/\/.git//g' | gum filter)
-cd $project
 
-id=$project
-tmux new-session -d -s $id
-tmux send 'pull' ENTER
-tmux attach -t $id
+if [ -z "$project" ]
+then
+  echo "You need to select a project."
+else  
+  cd $project
+  id=$project
+    
+  if [ -z "$TMUX" ]
+  then
+    tmux new-session -d -s $id
+    tmux send 'pull' ENTER
+    tmux attach -t $id
+  else  
+    tmux switch-client $id
+  fi
+fi
