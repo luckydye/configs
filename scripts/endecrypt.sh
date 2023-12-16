235cc5afc73b4a4b11a59361f10999b6143d32c7
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-12-16 17:04:01 +0100

    Create endecrypt.sh

---
+++ scripts/endecrypt.sh
@@ -0,0 +1,10 @@
+export function encrypt() {
+  openssl enc -aes-256-cbc -salt -in $1 -out $1.enc
+}
+
+export function decrypt() {
+  replace=".enc"
+  replacewith=""
+  out="${1/${replace}/${replacewith}}"
+  openssl enc -d -aes-256-cbc -in $1 -out $out
+}
