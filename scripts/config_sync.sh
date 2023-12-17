caa33f19a2596f6a71955f859d6eea60d1df0298
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-12-17 04:14:43 +0100

    sync config

--- scripts/config_sync.sh
+++ scripts/config_sync.sh
@@ -1,26 +1,24 @@
-#!/bin/bash
-
-echo "Stash local changes and pull..." 
+echo "Stash local changes and pull..."
 
 git -C $CONFIGS_DIR/ add --all
 git -C $CONFIGS_DIR/ stash
 git -C $CONFIGS_DIR/ pull
 git -C $CONFIGS_DIR/ stash pop
 
-echo "Rerun setup..." 
+echo "Rerun setup..."
 
 jetp local -p $CONFIGS_DIR/setup.yml || exit 1
 
-echo "Encrypt env..." 
+echo "Encrypt env..."
 sleep 1
 
 enc $CONFIGS_DIR/env
 
 sleep 1
 
-echo "Push configs..." 
+echo "Push configs..."
 git -C $CONFIGS_DIR/ add --all
 git -C $CONFIGS_DIR/ commit -m "sync config"
 git -C $CONFIGS_DIR/ push
 
-echo "Done" 
+echo "Done"
