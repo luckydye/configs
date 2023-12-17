d93a23ec84950620055f441c98e7546799befcc2
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-12-17 03:34:19 +0100

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -10,7 +10,4 @@ export RTX_INSTALL_PATH=~/bin/rtx
 
 export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix'
 
-export CHARM_HTTP_PORT=443
-export CHARM_HOST=charm.luckydye.de
-
 source $CONFIGS_DIR/env 2> /dev/null
