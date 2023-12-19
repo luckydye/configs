4067da9a241586e444792d9665bfefbef23d9ab2
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-12-19 22:19:02 +0100

    Update config_sync.sh

--- scripts/config_sync.sh
+++ scripts/config_sync.sh
@@ -1,18 +1,16 @@
-alias cfg='/usr/bin/git -C $CONFIGS_DIR/'
-
 function push_changes() {
   echo "Push configs..."
-  cfg add --all
-  cfg commit -m "sync config"
-  cfg push
+  git -C $CONFIGS_DIR/ add --all
+  git -C $CONFIGS_DIR/ commit -m "sync config"
+  git -C $CONFIGS_DIR/ push
 }
 
 echo "Stash local changes and pull..."
 
-cfg add --all
-cfg stash
-cfg pull
-cfg stash pop
+git -C $CONFIGS_DIR/ add --all
+git -C $CONFIGS_DIR/ stash
+git -C $CONFIGS_DIR/ pull
+git -C $CONFIGS_DIR/ stash pop
 
 echo "Encrypt env..."
 
@@ -25,11 +23,11 @@ jetp local -p $CONFIGS_DIR/setup.yml || exit 1
 echo "Merge env..."
 
 # merge new env with old env.swap
-cfg merge-file -p env env.swap env > env
+git -C $CONFIGS_DIR/ merge-file -p env env.swap env > env
 enc $CONFIGS_DIR/env
 
 echo "Push changes..."
 
-cfg diff --exit-code || push_changes
+git -C $CONFIGS_DIR/ diff --exit-code || push_changes
 
 echo "Done"
