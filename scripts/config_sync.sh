1515feb688d49435a27a9f409bf6b02c1a096869
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-01-02 19:26:38 +0100

    sync config

--- scripts/config_sync.sh
+++ scripts/config_sync.sh
@@ -1,10 +1,10 @@
 source $CONFIGS_DIR/commands.sh
 
 function push_changes() {
-  echo "Push configs..."
-  git -C $CONFIGS_DIR/ add --all
-  git -C $CONFIGS_DIR/ commit -m "sync config"
-  git -C $CONFIGS_DIR/ push
+	echo "Push configs..."
+	git -C $CONFIGS_DIR/ add --all
+	git -C $CONFIGS_DIR/ commit -m "sync config"
+	git -C $CONFIGS_DIR/ push
 }
 
 echo "Stash local changes and pull..."
@@ -13,14 +13,14 @@ git -C $CONFIGS_DIR/ stash
 git -C $CONFIGS_DIR/ pull
 git -C $CONFIGS_DIR/ stash pop
 
-cp env env.swap
+cp $CONFIGS_DIR/env $CONFIGS_DIR/env.swap
 
 echo "Rerun setup..."
 jetp local -p $CONFIGS_DIR/setup.yml || exit 1
 
 echo "Merge env..."
 # merge new env with old env.swap
-git -C $CONFIGS_DIR/ merge-file -p $CONFIGS_DIR/env $CONFIGS_DIR/env $CONFIGS_DIR/env.swap > $CONFIGS_DIR/env
+git -C $CONFIGS_DIR/ merge-file -p $CONFIGS_DIR/env $CONFIGS_DIR/env $CONFIGS_DIR/env.swap >$CONFIGS_DIR/env
 rm $CONFIGS_DIR/env.swap
 
 echo "Encrypt env..."
