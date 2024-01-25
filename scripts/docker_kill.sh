2f3e73d8f5e6c50c0444ffba22293265d43facb6
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-01-25 21:33:47 +0100

    Update docker_kill.sh

--- scripts/docker_kill.sh
+++ scripts/docker_kill.sh
@@ -1,2 +1,2 @@
-container=$(docker ps --format '{{.ID}} - {{.Names}} - {{.Status}}' | gum choose --no-limit | cut -d' ' -f 1) && \
+container=$(docker ps --all --format '{{.ID}} - {{.Names}} - {{.Status}}' | gum choose --no-limit | cut -d' ' -f 1) && \
 docker container remove -f $container
