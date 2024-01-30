d34b4914f93eecc4059fcf5c80bbb61c02e4b218
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-01-30 19:51:15 +0100

    sync config

---
+++ scripts/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Test
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName scripts
+
+echo "Hello World!"
