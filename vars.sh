08802538dce890e7256d06502744ea2a2dcbbb4a
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-11-24 20:01:28 +0100

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -13,4 +13,6 @@ export MISE_TRUSTED_CONFIG_PATHS=~$HOME/.config/mise/config.toml
 export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix'
 export FZF_DEFAULT_OPTS='--height=100% --info=inline --preview-window="up,70%,border-horizontal"'
 
+export GIT_LFS_SKIP_SMUDGE=1
+
 source $CONFIGS_DIR/env 2>/dev/null
